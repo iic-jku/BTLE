5417c37be277b38a756779337e704597cc64166a
Harald Pretl <harald@pretl.eu>
2024-12-02 21:31:45 +0100

    Define additional conditional controls

--- verilog/btle_config.v
+++ verilog/btle_config.v
@@ -6,8 +6,12 @@
 `define __BTLE_CONFIG__
 `timescale 1ns / 1ps
 // BTLE configurations
-// if TX_IQ is defined then an IQ TX interface is generated
+// if BTLE_TX_IQ is defined then an IQ TX interface is generated
 //`define BTLE_TX_IQ
-// if TX_POLAR is defined then a polar TX interface is generated
+// if BTLE_TX_POLAR is defined then a polar TX interface is generated
 `define BTLE_TX_POLAR
+// if BTLE_SYNTH is defined then the RX and TX channel number interface is generated
+`define BTLE_SYNTH
+// if BTLE_BAREMETAL is defined then the baremetal PHY interface is generated
+//`define BTLE_BAREMETAL
 `endif
