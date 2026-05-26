a6de5917b5e8d22021186a8a5b30f8fe791947e5
Xianjun Jiao <putaoshu@msn.com>
2026-05-26 16:37:37 +0200

    Make iq_to_bin_file.m also accept .wav file

--- host/ble_fpga_ctl/iq_to_bin_file.m
+++ host/ble_fpga_ctl/iq_to_bin_file.m
@@ -1,6 +1,32 @@
 function iq_to_bin_file(a)
 % bin file: copmlex, int16, i/q interleaved
-fid = fopen('iq.bin', 'w');
+% if a is a .wav file saved by sdr++, it should be saved in int16 format
+
+bin_filename = 'iq.bin';
+
+wav_file_input = 0;
+if isstr(a) % a might be a filename
+  [filepath, name, ext] = fileparts(a);
+  if ~strcmpi(ext, '.wav')
+    disp('The filename should be .wav. Saved via sdr++ in int16 format!');
+    return;
+  end
+  [Y, FS] = audioread(a, 'native');
+  data_type = class(Y);
+  if ~strcmpi(data_type, 'int16')
+    disp('The input file should be saved via sdr++ in int16 format!');
+    return;
+  end
+  a = Y;
+  clear Y;
+  disp(['Sampling rate ' num2str(FS)]);
+  bin_filename = [filepath '/' name '.bin'];
+  wav_file_input = 1;
+end
+
+disp(bin_filename);
+
+fid = fopen(bin_filename, 'w');
 if fid == -1
     disp('fopen failed!');
     return;
@@ -17,10 +43,13 @@ if num_col == 2
 end
 
 a = a(:);
-max_val = max(abs(a));
-scaling_factor = 30000/max_val;
-disp(['max ' num2str(max_val) ' scaling factor ' num2str(scaling_factor)]);
-a = int16(a.*scaling_factor);
+
+if wav_file_input == 0
+  max_val = max(abs(a));
+  scaling_factor = 30000/max_val;
+  disp(['max ' num2str(max_val) ' scaling factor ' num2str(scaling_factor)]);
+  a = int16(a.*scaling_factor);
+end
 
 fwrite(fid, a, 'int16');
 fclose(fid);
